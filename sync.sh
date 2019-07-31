da077191f56004b2b9fc9593e2eb144c8e1ffeaa
Thomas Leese <thomas@leese.io>
2019-07-31 19:41:38 +0100

    Add clean to pg_dump

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Dumping the production database"
-pg_dump $PRODUCTION_DATABASE_URL -f dump.out --format c
+pg_dump $PRODUCTION_DATABASE_URL -f dump.out --format c --clean
 
 echo "Restoring the staging database"
 pg_restore -d $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner
