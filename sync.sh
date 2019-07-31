8f6dfe5933fc9518ce031cdfa40210cb8425df92
Thomas Leese <thomas@leese.io>
2019-07-31 19:35:03 +0100

    Use custom format

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Dumping the production database"
-pg_dump $PRODUCTION_DATABASE_URL -f dump.sql
+pg_dump $PRODUCTION_DATABASE_URL -f dump.out -F c
 
 echo "Restoring the staging database"
-psql $STAGING_DATABASE_URL -f dump.sql
+pg_restore $STAGING_DATABASE_URL dump.out --clean
