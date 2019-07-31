fbd5f90348b5b0151521768a6f1617fcf44409e3
Thomas Leese <thomas@leese.io>
2019-07-31 19:31:26 +0100

    Fix syncing script

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ echo "Dumping the production database"
 pg_dump $PRODUCTION_DATABASE_URL -f dump.sql
 
 echo "Restoring the staging database"
-pg_restore $STAGING_DATABASE_URL dump.sql
+psql $STAGING_DATABASE_URL -f dump.sql
