12ce6d9561a99d750a59f8213f8cdb1ced60f14f
Thomas Leese <thomas@leese.io>
2019-07-31 19:37:38 +0100

    Fix pg_restore

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ echo "Dumping the production database"
 pg_dump $PRODUCTION_DATABASE_URL -f dump.out --format c
 
 echo "Restoring the staging database"
-pg_restore $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner
+pg_restore -d $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner
