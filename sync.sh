fc8872cd8878e4dd09eee0a9ef55d178e61d1bf5
Thomas Leese <thomas@leese.io>
2019-07-31 19:35:58 +0100

    No owner and no acl

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Dumping the production database"
-pg_dump $PRODUCTION_DATABASE_URL -f dump.out -F c
+pg_dump $PRODUCTION_DATABASE_URL -f dump.out --format c
 
 echo "Restoring the staging database"
-pg_restore $STAGING_DATABASE_URL dump.out --clean
+pg_restore $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner
