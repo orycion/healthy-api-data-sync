d18510dc077839b565ffe9a6fe342d6674aea131
Thomas Leese <thomas@leese.io>
2020-09-12 20:43:07 +0100

    Remove schema specification

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ echo "Dumping the production database"
 pg_dump $PRODUCTION_DATABASE_URL -f dump.out --format c --clean -T public.spatial_ref_sys
 
 echo "Restoring the staging database"
-pg_restore -d $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner -n public
+pg_restore -d $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner
