3e620b7e0149f42a64c87de0595ffac93ca03716
Thomas Leese <thomas@leese.io>
2020-09-12 13:13:15 +0100

    Don't dump public.spatial_ref_sys

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Dumping the production database"
-pg_dump $PRODUCTION_DATABASE_URL -f dump.out --format c --clean
+pg_dump $PRODUCTION_DATABASE_URL -f dump.out --format c --clean -T public.spatial_ref_sys
 
 echo "Restoring the staging database"
 pg_restore -d $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner -n public
