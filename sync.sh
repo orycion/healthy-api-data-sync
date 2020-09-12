3cbf3ee2cc365b00dae339c794a0ad64a0baadd7
Thomas Leese <thomas@leese.io>
2020-09-12 13:09:24 +0100

    Only sync public schemas
    This prevents issues in Heroku where the dump tries to create an
    extension.

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ echo "Dumping the production database"
 pg_dump $PRODUCTION_DATABASE_URL -f dump.out --format c --clean
 
 echo "Restoring the staging database"
-pg_restore -d $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner
+pg_restore -d $STAGING_DATABASE_URL dump.out --clean --no-acl --no-owner -n public
